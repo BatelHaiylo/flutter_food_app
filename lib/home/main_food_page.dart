218c6f6953be885734c8289c7f6c83d5154b4568
Batel Haiylo <inpo686@gmail.com>
2022-11-10 21:49:10 +0200

    yehuda

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,33 +1,39 @@
-class MainFoodPage extends StatefulWidget{
-  const MainFoodPage({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
   @override
-  _MainFoodPageState createState() => _MainFoodPage();
+  State<MainFoodPage> createState() => _MainFoodPage();
 }
 
-class _MainFoodPageState extends State<MainFoodPage> {
+class _MainFoodPage extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(),
-      child: Container(
-        child: Row(
-          children:
-          Column(
+      body: Container(
+        child: Container(
+          child: Row(
             children: [
-              Text("country"),
-              Text("city")
+              Column(
+                children: [
+                  Text("Country"),
+                  Text("City")
+                ],
+              ),
+              Container(
+                width: 45,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blue,
+                ),
+              )
             ],
           ),
-          Container(
-            width: 45,
-            height: 45,
-            decoration:BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.blue,
-            ),
-
-          )
-        )
+        ),
       ),
     );
   }
